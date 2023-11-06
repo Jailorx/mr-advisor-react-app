ed35bbef1fde93c4eec2f2fe8f2f9df1a19eb105
Shubham Bisht <bisht.shubham.emp@gmail.com>
2023-11-07 00:23:09 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import React from "react";
+import axios from "axios";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [advice, setAdvice] = useState("");
+  const baseUrl = `https://api.adviceslip.com/advice`;
+
+  useEffect(() => {
+    fetchAdvice();
+  }, []);
+
+  const fetchAdvice = () => {
+    axios
+      .get(baseUrl)
+      .then((response) => {
+        const { advice } = response.data.slip;
+        setAdvice(advice);
+      })
+      .catch((error) => console.log(error));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="card">
+        <div className="heading">
+          <h1>{advice}</h1>
+        </div>
+        <button className="button" onClick={() => fetchAdvice()}>
+          More Advice
+        </button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
